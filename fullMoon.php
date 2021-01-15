5aef12159a178f694ff2fb92b135be5fe9891a05
Leonel Godoy <godoyutn@gmail.com>
2021-01-15 02:30:55 +0100

    Primer commit HTML

---
+++ fullMoon.php
@@ -0,0 +1,30 @@
+Full Moon
+Nature Boy, Buddy Merrill, ...
+To live in an old shack by the sea
+(And breathe the sweet salt air)
+To live with the dawn and the dusk
+The new moon and the full moon
+The tides the wind and the rain...
+To surf and comb the beach
+And gather drift-wood and shells
+And know the thrill of loneliness
+And lose all sense of time
+And be free
+To hike over the island to the village
+And visit the marketplace
+And enjoy the music and the food and the people
+And do a little trading
+And see the great ships come and go
+And in the evening
+(When the sky is on fire)
+Heaven and earth become my great open cathedral
+Where all men are brothers
+Where all things are bound by law
+And crowned with love
+Poor, alone and happy
+I make a fire on the beach
+And as darkness covers the face of the deep
+Lie down in the wild grass
+And dream the dream that the dreamers dream
+I am the wind, the sea, the evening star
+I am everyone, anyone, no one
